b4c7139d246350c99008c0195c2e60ef1257b952
pushpdeep <>
2024-11-25 06:22:53 +0000

    added payload

---
+++ experiment/simulation/js/ts_simulation/data.js
@@ -0,0 +1,31 @@
+// ====== Activity 1 variables ====== //
+let a_a1;
+let b_a1;
+let I_a1;
+let u1_a1 = 1 / Math.sqrt(3);
+let u2_a1 = -1 / Math.sqrt(3);
+let x1_a1;
+let x2_a1;
+let x_u1_a1;
+let x_u2_a1;
+// ====== Activity 2 variables ====== //
+let a_a2;
+let b_a2;
+let I_a2;
+let u1_a2 = 1 / Math.sqrt(3);
+let u2_a2 = -1 / Math.sqrt(3);
+let x1_a2;
+let x2_a2;
+let x_u1_a2;
+let x_u2_a2;
+// ====== Activity 3 variables ====== //
+let a_a3;
+let b_a3;
+let I_a3;
+let u1_a3 = 1 / Math.sqrt(3);
+let u2_a3 = -1 / Math.sqrt(3);
+let x1_a3;
+let x2_a3;
+let x_u1_a3;
+let x_u2_a3;
+//# sourceMappingURL=data.js.map
---
+++ experiment/simulation/js/ts_simulation/gauss_quad_2pt.js
@@ -0,0 +1,16 @@
+function f1(x) {
+    return Math.pow(x, 3) - x + 1;
+}
+function f2(x) {
+    return 2 * Math.pow(x, 2) + 1;
+}
+function f3(x) {
+    return 1 / (1 + Math.pow(x, 2));
+}
+function get_a(x1, x2) {
+    return (x2 - x1) / 2;
+}
+function get_b(x1, x2) {
+    return (x2 + x1) / 2;
+}
+//# sourceMappingURL=gauss_quad_2pt.js.map
